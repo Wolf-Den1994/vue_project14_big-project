3802e57e459285cbd606ceb6e8c3de99e1c6d20a
Denis <wolf-den@tut.by>
2022-01-05 23:42:49 +0300

    sidebar

--- src/layout/MainLayout.vue
+++ src/layout/MainLayout.vue
@@ -1,5 +1,6 @@
 <template>
-  <the-navbar></the-navbar>
+  <the-sidebar />
+  <the-navbar />
   <div class="container with-nav">
     <app-message />
     <router-view />
@@ -9,10 +10,13 @@
 <script>
 import TheNavbar from "@/components/TheNavbar";
 import AppMessage from "@/components/ui/AppMessage";
+import TheSidebar from "@/components/TheSidebar";
+
 export default {
   components: {
     TheNavbar,
-    AppMessage
+    AppMessage,
+    TheSidebar,
   }
 }
 </script>
